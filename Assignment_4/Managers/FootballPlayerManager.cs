7962322ab1a55b9408af7a34f6b157ceb7c7e6dd
KengoKato <kengo.kato15@gmail.com>
2022-10-03 11:10:34 +0200

    .

---
+++ Assignment_4/Managers/FootballPlayerManager.cs
@@ -0,0 +1,30 @@
+﻿namespace Assignment_4.Managers
+{
+    public class FootballPlayerManager
+    {
+        private static List<FootballPlayer> = new List<FootballPlayer>();
+        
+        
+
+        public void GetAll()
+        {
+
+        }
+        public void GetByID()
+        {
+
+        }
+        public void Add()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+        public void Delete()
+        {
+
+        }
+    }
+}
