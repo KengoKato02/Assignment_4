315f883f11ea6f4aa38ffa989e7bfa0a3c6df106
KengoKato <kengo.kato15@gmail.com>
2022-10-04 09:45:24 +0200

    Update from PC

--- Assignment_4/Managers/FootballPlayerManager.cs
+++ Assignment_4/Managers/FootballPlayerManager.cs
@@ -1,30 +1,60 @@
-﻿namespace Assignment_4.Managers
+﻿using MandaAssig_1;
+
+namespace Assignment_4.Managers
 {
     public class FootballPlayerManager
     {
-        private static List<FootballPlayer> = new List<FootballPlayer>();
-        
-        
-
-        public void GetAll()
+        private static int _nextId = 1;
+        private int Id = 0;
+        private static readonly List<FootballPlayer> Data = new List<FootballPlayer>
         {
+            new FootballPlayer {Id = _nextId++, Name = "Ronaldo", Age = 32, shirtNumber = 7},
+            new FottballPlayer {Id = _nextId++, Name = "Messi", Age = 28, shirtNumber = 10},
+            new FottballPlayer {Id = _nextId++, Name = "Kacper", Age = 20, shirtNumber = 99},
+            new FottballPlayer {Id = _nextId++, Name = "Pawel", Age = 90, shirtNumber = 69}
+            //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/object-and-collection-initializers
+        };
+
+        public List<FootballPlayer> GetAll()
+        {
+            return new List<FootballPlayer>(Data);
+        }
+        public FootballPlayer GetByID(int id)
+        {
+            return Data.Find(player => player.Id == id);
+        }
+        public FootballPlayer Add(FootballPlayer newPlayer)
+        {
+            newPlayer.Id = _nextId++;
+            Data.Add(newPlayer);
+            return newPlayer;
+        }
+        public FootballPlayer Update(int id, FootballPlayer updates)
+        {
+            FootballPlayer player = Data.Find(player1 => player1.Id == id);
+            if (player == null)
+            {
+                return null;
+            }
+            else
+            {
+                Data.Remove(player);
+                return player;
+            }
 
         }
-        public void GetByID()
+        public FootballPlayer Delete(int id)
         {
-
-        }
-        public void Add()
-        {
-
-        }
-        public void Update()
-        {
-
-        }
-        public void Delete()
-        {
-
+            FootballPlayer book = Data.Find(player1 => player1.Id == id);
+            if (book == null)
+            {
+                return null;
+            }
+            else
+            {
+                Data.Remove(book);
+                return book;
+            }
         }
     }
 }
---
+++ Assignment_4Tests/Managers/FootballPlayerManagerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment_4.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4.Managers.Tests
+{
+    [TestClass()]
+    public class FootballPlayerManagerTests
+    {
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            FootballPlayerManager manager = new FootballPlayerManager();
+
+            List<FootballPlayer> allPlayers = manager.GetAll();
+            Assert.AreEqual(2, allPlayers.Count);
+
+            FootballPlayer player = manager.GetByID(1);
+            Assert.AreEqual("Ronaldo", player.Title);
+
+            Assert.IsNull(manager.GetByID(100));
+
+            FootballPlayer newPlayer = new FootballPlayer { Name = "Pepe", Age = 33, shirtnumber = 54 };
+            FootballPlayer addedPlayer = manager.Add(newPlayer);
+            Assert.AreEqual(3, addedPlayer.Id);
+            Assert.AreEqual(3, manager.GetAll().Count);
+
+            FootballPlayer updates = new FootballPlayer { Name = "Modric", Age = 35, shirtnumber = 17 };
+            FootballPlayer updatedPlayer = manager.Update(3, updates);
+            Assert.AreEqual("Modric", updatedBook.Title);
+
+            Assert.IsNull(manager.Update(100, updates));
+
+            FootballPlayer deletedPlayer = manager.Delete(3);
+            Assert.AreEqual("Modric", deletedPlayer.Title);
+            Assert.AreEqual(2, manager.GetAll().Count);
+
+            Assert.IsNull(manager.Delete(100));
+        }
+    }
+}
